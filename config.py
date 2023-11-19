88537aaaa96ade42915484a2e0fdd7a820bec481
arezvani <fcairib76@gmail.com>
2023-11-19 20:07:12 +0330

    Update config.py

--- config.py
+++ config.py
@@ -1,6 +1,6 @@
 # API
 api_url = 'https://api.livecoinwatch.com/exchanges/list'
-token = '865087d8-64cb-409c-81e8-4341b5bd7c6f'
+token = '******-****-****-****-*******'
 header = {
     'x-api-key': token
 }
@@ -15,17 +15,17 @@ data = {
 
 
 # Kafka
-kafka_broker = 'dbaas.abriment.com:32744'
-kafka_topic = 'data'
+kafka_broker = '**********:****'
+kafka_topic = '***'
 
 # DB
-db_host = 'dbaas.abriment.com'
-db_port = '31811'
-db_database = 'process'
+db_host = '**************'
+db_port = '********'
+db_database = '*******'
 db_conn = f'jdbc:postgresql://{db_host}:{db_port}/{db_database}'
-db_username = 'postgres'
-db_password = '7rS86n2Z'
-db_table = 'aggregates'
+db_username = '*******'
+db_password = '*********'
+db_table = '*********'
 
 # Spark
 checkpoint_location = '/tmp/spark-local-dir-1'
